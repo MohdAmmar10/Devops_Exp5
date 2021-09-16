8f1ff64f15017b8b683adacd8b06eb45e6e49e64
MohdAmmar10 <mohd.ammarmakki786@gmail.com>
2021-09-16 14:39:18 +0530

    Upated pattern

--- PatternPrint.java
+++ PatternPrint.java
@@ -5,11 +5,15 @@ public class PatternPrint
         int i, j, row=6;   
         for(i=0; i<row; i++)   
         {   
-            for(j=0; j<=i; j++)   
+            for(j=2*(row-i); j>=0; j--) 
+            {
+                System.out.print(" ");   
+            }   
+            for (j=0; j<=i; j++ )   
             {   
                 System.out.print("* ");   
             }   
-            System.out.println();   
+                System.out.println();   
         }   
     }   
 }  
